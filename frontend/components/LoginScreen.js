b4c7912a2a20dbf02a71b0cce08f8388f7eee88b
Daniel Espinal <dancancode@protonmail.com>
2022-05-06 17:41:54 -0400

    fixed Login screen

--- frontend/components/LoginScreen.js
+++ frontend/components/LoginScreen.js
@@ -1,30 +1,55 @@
-import React from 'react';
-import { Box, Text, Heading, VStack, FormControl, Input, Link, Button, HStack, Center,Image,View } from "native-base";
-function LoginScreen({navigation}) {
-  return <Center w="100%">
+import React from "react";
+import {
+  Box,
+  Text,
+  Heading,
+  VStack,
+  FormControl,
+  Input,
+  Link,
+  Button,
+  HStack,
+  Center,
+  Image,
+  View,
+} from "native-base";
+function LoginScreen({ navigation }) {
+  return (
+    <Center w="100%">
       <Box safeArea p="2" py="8" w="90%" maxW="290">
-        <Heading size="lg" fontWeight="600" color="coolGray.800" _dark={{
-        color: "warmGray.50"
-      }}>
+        <Heading
+          size="lg"
+          fontWeight="600"
+          color="coolGray.800"
+          _dark={{
+            color: "warmGray.50",
+          }}
+        >
           Welcome
         </Heading>
-        <Heading mt="1" _dark={{
-        color: "warmGray.200"
-      }} color="coolGray.600" fontWeight="medium" size="xs">
+        <Heading
+          mt="1"
+          _dark={{
+            color: "warmGray.200",
+          }}
+          color="coolGray.600"
+          fontWeight="medium"
+          size="xs"
+        >
           Sign in to continue!
         </Heading>
-        <View style={{alignItems: 'center'}}>
-              <Image
-                alt="description of image"
-                source={require('../assets/logo.png')}
-                style={{
-                  width: '70%',
-                  height: 200,
-                  resizeMode: 'cover',
-                  margin: 30,
-                }}
-              />
-            </View>
+        <View style={{ alignItems: "center" }}>
+          <Image
+            alt="description of image"
+            source={require("../../assets/logo.png")}
+            style={{
+              width: "70%",
+              height: 200,
+              resizeMode: "cover",
+              margin: 30,
+            }}
+          />
+        </View>
 
         <VStack space={3} mt="5">
           <FormControl>
@@ -34,11 +59,15 @@ function LoginScreen({navigation}) {
           <FormControl>
             <FormControl.Label>Password</FormControl.Label>
             <Input type="password" />
-            <Link _text={{
-            fontSize: "xs",
-            fontWeight: "500",
-            color: "indigo.500"
-          }} alignSelf="flex-end" mt="1">
+            <Link
+              _text={{
+                fontSize: "xs",
+                fontWeight: "500",
+                color: "indigo.500",
+              }}
+              alignSelf="flex-end"
+              mt="1"
+            >
               Forget Password?
             </Link>
           </FormControl>
@@ -46,22 +75,30 @@ function LoginScreen({navigation}) {
             Sign in
           </Button>
           <HStack mt="6" justifyContent="center">
-            <Text fontSize="sm" color="coolGray.600" _dark={{
-            color: "warmGray.200"
-          }}>
+            <Text
+              fontSize="sm"
+              color="coolGray.600"
+              _dark={{
+                color: "warmGray.200",
+              }}
+            >
               I'm a new user.{" "}
             </Text>
-            <Link onPress={() => navigation.navigate('RegisterScreen')} _text={{
-            color: "indigo.500",
-            fontWeight: "medium",
-            fontSize: "sm"
-          }} >
+            <Link
+              onPress={() => navigation.navigate("RegisterScreen")}
+              _text={{
+                color: "indigo.500",
+                fontWeight: "medium",
+                fontSize: "sm",
+              }}
+            >
               Sign Up
             </Link>
           </HStack>
         </VStack>
       </Box>
-    </Center>;
+    </Center>
+  );
 }
 
 export default LoginScreen;
