2b335ad8e56c73aeabdd8eedf2b0e26db3fb2e88
brandon-man <nybrandonman@gmail.com>
2022-05-10 21:33:12 -0700

    Workouts slice

--- frontend/components/WorkoutsScreen.js
+++ frontend/components/WorkoutsScreen.js
@@ -1,12 +1,12 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
 
 const WorkoutsScreen = () => {
   return (
-    <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>WorkoutsScreen</Text>
     </View>
-  )
-}
+  );
+};
 
-export default WorkoutsScreen
+export default WorkoutsScreen;
