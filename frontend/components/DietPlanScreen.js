f049390e49971314c25a24848a4ba32d028ab93d
Daniel Espinal <dancancode@protonmail.com>
2022-05-17 17:26:36 -0400

    added isPressed visuals

--- frontend/components/DietPlanScreen.js
+++ frontend/components/DietPlanScreen.js
@@ -12,6 +12,7 @@ import { Dimensions } from "react-native";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchDiets } from "../store/slices/diets.slice.js";
+import { transform } from "lodash";
 
 const DietPlanScreen = () => {
   const dispatch = useDispatch();
@@ -41,21 +42,33 @@ const DietPlanScreen = () => {
                   handleClick(diet.id);
                 }}
               >
-                <Box
-                  shadow={3}
-                  margin={2}
-                  boxSize="150"
-                  //backgroundColor={isPressed ? "#ff00ff" : "#556B2F"}
-                >
-                  <Text
-                    textAlign="center"
-                    color="#ffffff"
-                    fontWeight="bold"
-                    fontSize="18"
-                  >
-                    {diet.name}
-                  </Text>
-                </Box>
+                {({ isPressed }) => {
+                  return (
+                    <Box
+                      shadow={3}
+                      margin={2}
+                      boxSize="150"
+                      backgroundColor={isPressed ? "#008b8b" : "#556B2F"}
+                      rounded="8"
+                      style={{
+                        transform: [
+                          {
+                            scale: isPressed ? 0.94 : 1,
+                          },
+                        ],
+                      }}
+                    >
+                      <Text
+                        textAlign="center"
+                        color="#ffffff"
+                        fontWeight="bold"
+                        fontSize="18"
+                      >
+                        {diet.name}
+                      </Text>
+                    </Box>
+                  );
+                }}
               </Pressable>
             );
           })}
